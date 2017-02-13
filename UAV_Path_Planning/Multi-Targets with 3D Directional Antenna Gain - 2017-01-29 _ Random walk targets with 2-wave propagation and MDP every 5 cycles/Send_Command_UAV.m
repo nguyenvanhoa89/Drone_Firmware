73b38d58ccb5d1816a93bb35b5bb80ebb80884cc
nguyenvanhoa89 <nguyenvanhoa89@gmail.com>
2017-02-13 22:37:47 +1030

    Remove mathlab draft files

--- UAV_Path_Planning/Multi-Targets with 3D Directional Antenna Gain - 2017-01-29 _ Random walk targets with 2-wave propagation and MDP every 5 cycles/Send_Command_UAV.m
+++
@@ -1,7 +0,0 @@
-URL = 'http://localhost:8000/';
-options = weboptions('MediaType','application/json');%,'RequestMethod','auto','ArrayFormat','json','ContentType','json');
-data = webread(URL);
-write_data.x = data.position(1) + 0.001;
-write_data.y = data.position(2) + 0.001;
-write_data.alt = data.position(3);
-response = webwrite('http://localhost:8000',write_data,options);
--- UAV_Path_Planning/Multi-Targets with 3D Directional Antenna Gain - 2017-02-01 _ Random walk targets with 2-wave propagation and MDP every 5 cycles_Tracking/Plot_RMS_Result_for_EachTarget.m
+++
@@ -1,9 +0,0 @@
-figure(3);
-for i = 1:ntarget
-    subplot(1,ntarget,i);
-    plot(1:k, est.RMS{i}(1,1:k),'-k');
-    title(['RMS of target ',num2str(i)]);
-    ylabel('RMS (m)');
-    xlabel('Cycle');
-    grid on;
-end
